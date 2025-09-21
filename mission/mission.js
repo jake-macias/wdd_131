43e5006fdc6f2c6609ba0a094ffeb8ac5a66540b
jake-macias <jakenysm@gmail.com>
2025-09-20 20:04:36 -0600

    all my edits

--- mission/mission.js
+++ mission/mission.js
@@ -1,4 +1,41 @@
-const apples = 10;
-const oranges = 5;
+const missionParagraph = document.createElement("p");
+missionParagraph.innerText =
+  "BYU–Idaho provides a high-quality education that fosters faith, integrity, and excellence, preparing students for lifelong learning, leadership, and service.";
+document.body.appendChild(missionParagraph);
 
-console.log(apples + oranges);
+const newImage = document.createElement("img");
+newImage.setAttribute("alt", "BYU–Idaho Logo");
+newImage.src =
+  "https://www.byui.edu/images/service-sites/map/byui-logo_blue.jpg"; // default: blue logo
+document.body.appendChild(newImage);
+
+const values = ["Faith", "Integrity", "Excellence"];
+
+const valuesDiv = document.createElement("div");
+valuesDiv.innerHTML = `
+  <h3>Our Values</h3>
+  <ul>
+    <li>${values[0]}</li>
+    <li>${values[1]}</li>
+    <li>${values[2]}</li>
+  </ul>
+`;
+document.body.appendChild(valuesDiv);
+
+const themeSelector = document.querySelector("#theme"); // dropdown element
+
+function changeTheme() {
+  if (themeSelector.value === "dark") {
+
+    document.body.classList.add("dark");
+    newImage.src =
+      "https://www.byui.edu/images/service-sites/map/byui-logo_white.png"; // white logo
+  } else {
+    document.body.classList.remove("dark");
+    newImage.src =
+      "https://www.byui.edu/images/service-sites/map/byui-logo_blue.jpg"; // blue logo
+  }
+}
+
+// Listen for theme changes
+themeSelector.addEventListener("change", changeTheme);
