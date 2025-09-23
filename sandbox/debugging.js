04ec8de9df9bc785480d894bf363ec5b842016a8
jake-macias <jakenysm@gmail.com>
2025-09-23 10:03:31 -0600

    add styles

--- sandbox/debugging.js
+++ sandbox/debugging.js
@@ -1,6 +1,12 @@
 const PI = 3.14;
-const radius = 3;
+let radius = 3;
 let area = 0;
-area = radius * radius * pi;
+function calculateArea(radius) {
+    const area = radius * radius * PI;
+}
+area = calculateArea(radius);
+area = radius * radius * PI;
+console.log("Area of circle with radius " + radius + " is: " + area);
 radius = 4;
-area = radius * radius * pi;
+area = radius * radius * PI;
+console.log("Area of circle with radius " + radius + " is: " + area);
