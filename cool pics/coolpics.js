ca6b5f040e5f2b2da6aea167af07a5ba62a9cbff
jake-macias <jakenysm@gmail.com>
2025-09-27 22:06:57 -0600

    done done i think

--- cool pics/coolpics.js
+++ cool pics/coolpics.js
@@ -16,29 +16,40 @@ function handleResize() {
 window.addEventListener("resize", handleResize);
 window.addEventListener("load", handleResize);
 
-function viewerTemplate(src, alt) {
+
+function popupTemplate(src, alt) {
   return `
-    <div class="viewer">
-      <button class="close-viewer">X</button>
+    <div class="popup" style="display:flex">
+      <button class="close-popup">X</button>
       <img src="${src}" alt="${alt}">
     </div>
   `;
 }
+
 
 function viewHandler(event) {
   if (event.target.tagName === "IMG") {
     let src = event.target.getAttribute("src");
     let alt = event.target.getAttribute("alt");
 
-    document.body.insertAdjacentHTML("afterbegin", viewerTemplate(src, alt));
+    document.body.insertAdjacentHTML("afterbegin", popupTemplate(src, alt));
 
-    document.querySelector(".close-viewer")
-      .addEventListener("click", closeViewer);
+    
+    document.querySelector(".close-popup")
+      .addEventListener("click", closePopup);
+
+ 
+    document.querySelector(".popup")
+      .addEventListener("click", function(e) {
+        if (e.target.classList.contains("popup")) {
+          closePopup();
+        }
+      });
   }
 }
 
-function closeViewer() {
-  document.querySelector(".viewer").remove();
+function closePopup() {
+  document.querySelector(".popup").remove();
 }
 
 document.querySelector(".gallery").addEventListener("click", viewHandler);
